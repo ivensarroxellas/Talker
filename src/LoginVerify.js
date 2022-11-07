5eb24afa739689570e85dcb54c9393172293e682
Ivens <ivensarroxellas@hotmail.com>
2022-11-07 13:04:19 -0300

    req 4- fix

--- src/LoginVerify.js
+++ src/LoginVerify.js
@@ -3,11 +3,11 @@ const emailVerify = (req, res, next) => {
   const regex = /\S+@\S+\.\S+/;
   
   if (!email) {
-    res.status(400).json({ message: 'O campo "email" é obrigatório' });
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
   
   if (!regex.test(email)) {
-    res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   
   next();
@@ -16,11 +16,11 @@ const emailVerify = (req, res, next) => {
 const passwordVerify = (req, res, next) => {
   const { password } = req.body;
   if (!password) {
-    res.status(400).json({ message: 'O campo "password" é obrigatório' });
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
   }
 
   if (password.length < 6) {
-    res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
   next();
 };
---
+++ src/addTalker.js
@@ -0,0 +1,1 @@
+const addTalker = async
